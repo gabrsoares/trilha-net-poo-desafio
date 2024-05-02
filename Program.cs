2ca5dd69554df256acca1d991e11b6e31cf71b13
Gabriel Soares <gabrsoares@outlook.com>
2024-05-02 19:06:01 -0300

    update code

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"123456", modelo:"modelo 1", imei:"1111111", memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("456789", "modelo 2", "222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Ifood");
